c049e08def31dd8e04d06211649154ebe2f01aca
siddheshwar-more <siddheshwar.more@opcito.com>
2016-03-05 20:08:15 +0530

    added test gulpfile.js

---
+++ auto-ecosys-staticresources/gulpfile.js
@@ -0,0 +1,13 @@
+
+In Penny Lane there is a barber showing photographs = Of every head he's had the pleasure to know
+And all the people that come and go = Stop and say hello
+
+On the corner is a banker with a motorcar = The little children laugh at him behind his back
+And the banker never wears a mack = In the pouring rain, very strange
+
+
+process.env.AUTO_ECOSYS_WEBAPP = http://localhost:8080/auto-ecosys-web/api
+
+
+Penny Lane is in my ears and in my e = esThere beneath the blue suburban skies
+I sit, and meanwhile =  back
